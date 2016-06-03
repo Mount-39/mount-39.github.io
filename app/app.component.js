3a32f2466f6aeda654783165550efba7b1a97abb
Artem Lanovyy <alanovyy@qualium-systems.com.ua>
2016-06-03 18:58:57 +0300

    First commit

---
+++ app/app.component.js
@@ -0,0 +1,38 @@
+System.register(['angular2/core'], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+        var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+        if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+        else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+        return c > 3 && r && Object.defineProperty(target, key, r), r;
+    };
+    var __metadata = (this && this.__metadata) || function (k, v) {
+        if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+    };
+    var core_1;
+    var AppComponent;
+    return {
+        setters:[
+            function (core_1_1) {
+                core_1 = core_1_1;
+            }],
+        execute: function() {
+            AppComponent = (function () {
+                function AppComponent() {
+                }
+                AppComponent = __decorate([
+                    core_1.Component({
+                        selector: 'my-app',
+                        // directives: [],
+                        template: "\t\t\t<section class=\"content bgcolor-4\">\n\t\t\t\t<h2>Mount 39</h2>\n\t\t\t\t<span class=\"input input--kozakura\">\n\t\t\t\t\t<input class=\"input__field input__field--kozakura\" type=\"text\" id=\"input-7\" />\n\t\t\t\t\t<label class=\"input__label input__label--kozakura\" for=\"input-7\">\n\t\t\t\t\t\t<span class=\"input__label-content input__label-content--kozakura\" data-content=\"Full Name\">Name</span>\n\t\t\t\t\t</label>\n\t\t\t\t\t<svg class=\"graphic graphic--kozakura\" width=\"300%\" height=\"100%\" viewBox=\"0 0 1200 60\" preserveAspectRatio=\"none\">\n\t\t\t\t\t\t<path d=\"M1200,9c0,0-305.005,0-401.001,0C733,9,675.327,4.969,598,4.969C514.994,4.969,449.336,9,400.333,9C299.666,9,0,9,0,9v43c0,0,299.666,0,400.333,0c49.002,0,114.66,3.484,197.667,3.484c77.327,0,135-3.484,200.999-3.484C894.995,52,1200,52,1200,52V9z\"/>\n\t\t\t\t\t</svg>\n\t\t\t\t</span>\n\t\t\t\t<span class=\"input input--kozakura\">\n\t\t\t\t\t<input class=\"input__field input__field--kozakura\" type=\"text\" id=\"input-8\" />\n\t\t\t\t\t<label class=\"input__label input__label--kozakura\" for=\"input-8\">\n\t\t\t\t\t\t<span class=\"input__label-content input__label-content--kozakura\" data-content=\"Email\">Email</span>\n\t\t\t\t\t</label>\n\t\t\t\t\t<svg class=\"graphic graphic--kozakura\" width=\"300%\" height=\"100%\" viewBox=\"0 0 1200 60\" preserveAspectRatio=\"none\">\n\t\t\t\t\t\t<path d=\"M1200,9c0,0-305.005,0-401.001,0C733,9,675.327,4.969,598,4.969C514.994,4.969,449.336,9,400.333,9C299.666,9,0,9,0,9v43c0,0,299.666,0,400.333,0c49.002,0,114.66,3.484,197.667,3.484c77.327,0,135-3.484,200.999-3.484C894.995,52,1200,52,1200,52V9z\"/>\n\t\t\t\t\t</svg>\n\t\t\t\t</span>\n\t\t\t\t<span class=\"input input--kozakura\">\n\t\t\t\t\t<input class=\"input__field input__field--kozakura\" type=\"number\" id=\"input-9\" />\n\t\t\t\t\t<label class=\"input__label input__label--kozakura\" for=\"input-9\">\n\t\t\t\t\t\t<span class=\"input__label-content input__label-content--kozakura\" data-content=\"Website\">Phone</span>\n\t\t\t\t\t</label>\n\t\t\t\t\t<svg class=\"graphic graphic--kozakura\" width=\"300%\" height=\"100%\" viewBox=\"0 0 1200 60\" preserveAspectRatio=\"none\">\n\t\t\t\t\t\t<path d=\"M1200,9c0,0-305.005,0-401.001,0C733,9,675.327,4.969,598,4.969C514.994,4.969,449.336,9,400.333,9C299.666,9,0,9,0,9v43c0,0,299.666,0,400.333,0c49.002,0,114.66,3.484,197.667,3.484c77.327,0,135-3.484,200.999-3.484C894.995,52,1200,52,1200,52V9z\"/>\n\t\t\t\t\t</svg>\n\t\t\t\t</span>\n\t\t\t</section>"
+                    }), 
+                    __metadata('design:paramtypes', [])
+                ], AppComponent);
+                return AppComponent;
+            }());
+            exports_1("AppComponent", AppComponent);
+        }
+    }
+});
+//# sourceMappingURL=app.component.js.map
